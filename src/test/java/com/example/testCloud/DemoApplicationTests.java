adbe14856ce833dc1256e68090493f4ccef13cbb
Anastasia <nastya.gudoshnikova@list.ru>
2025-04-09 10:12:52 +0400

    Initial commit

---
+++ src/test/java/com/example/testCloud/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.testCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class DemoApplicationTests {
+    @Test
+    void helloTest(){
+        assertEquals(1, 1);
+    }
+}
