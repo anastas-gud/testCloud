c6f745eaaf15d221bedc2b946d6a24890288ff10
Anastasia <nastya.gudoshnikova@list.ru>
2025-04-09 11:00:24 +0400

    Add tets2

---
+++ src/test/java/com/example/testCloud/DemoApplicationTests2.java
@@ -0,0 +1,16 @@
+package com.example.testCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class DemoApplicationTests2 {
+    @Test
+    void helloTest(){
+        assertEquals(2, 2);
+    }
+}
